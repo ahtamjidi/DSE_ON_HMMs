06267c0f85fa3a44823871916338305a53c3b72f
Reza Oftadeh <reza.oftadeh@tamu.edu>
2018-03-02 02:25:16 -0600

    Add PorjMetric

--- HMM_Extended_3A/PerfMeas_GST.m
+++ HMM_Extended_3A/PerfMeas_GST.m
@@ -7,6 +7,8 @@ function PM_GST = PerfMeas_GST(Global,Network,Network_GST)
             PM_GST.KLD(i,k) = KLD(Network_GST.Node(i).Post(:,k),Network.Node(i).Post(:,k));
             PM_GST.dcorr(i,k) = dcorr(Network_GST.Node(i).Post(:,k),Network.Node(i).Post(:,k));
             PM_GST.TVD(i,k) = 1/2*max(abs(Network_GST.Node(i).Post(:,k)-Network.Node(i).Post(:,k)));
+            temp = Network_GST.Node(i).Post(:,k)./Network.Node(i).Post(:,k);
+            PM_GST.ProjMetric(i,k) = log(max(temp)/min(temp));
         end
     end
     PM_GST.meanBCS = mean(PM_GST.BCS);
@@ -14,4 +16,5 @@ function PM_GST = PerfMeas_GST(Global,Network,Network_GST)
     PM_GST.meanKLD = mean(PM_GST.KLD);
     PM_GST.meandcorr = mean(PM_GST.dcorr);
     PM_GST.meanTVD = mean(PM_GST.TVD);
+    PM_GST.meanProjMetric = mean(PM_GST.ProjMetric);
 end
