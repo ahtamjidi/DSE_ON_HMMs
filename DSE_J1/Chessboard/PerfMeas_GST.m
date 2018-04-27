26e85f28abdd1dd8c2cc01ef9c8bdb036cb2d3fe
Amirhossein Tamjidi <ahtamjidi@gmail.com>
2018-04-27 06:02:45 -0600

    adding L1 norm

--- DSE_J1/Chessboard/PerfMeas_GST.m
+++ DSE_J1/Chessboard/PerfMeas_GST.m
@@ -4,11 +4,15 @@ function PM_GST = PerfMeas_GST(Sim,Network,Network_GST)
             PM_GST.Node(i).CM(k) = ClosenessMeasures(Network_GST.Node(i).Post(:,k),Network.Node(i).Post(:,k));
             PM_GST.BCS(i,k) = PM_GST.Node(i).CM(k).BCS;
             PM_GST.HEL(i,k) = PM_GST.Node(i).CM(k).HEL;
+            PM_GST.L1(i,k) = PM_GST.Node(i).CM(k).L1;
+
+            
             temp = Network_GST.Node(i).Post(:,k)./Network.Node(i).Post(:,k);
             PM_GST.ProjMetric(i,k) = log(max(temp)/min(temp));              
         end        
     end
     PM_GST.meanBCS = mean(PM_GST.BCS);
     PM_GST.meanHEL = mean(PM_GST.HEL);
+    PM_GST.meanL1 = mean(PM_GST.L1);
     PM_GST.meanProjMetric = mean(PM_GST.ProjMetric);  
 end
